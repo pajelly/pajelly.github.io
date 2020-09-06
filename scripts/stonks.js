e2798909b495ad3d786b1009931a3fbdca03a46c
peijia24 <peijia.dng@gmail.com>
2020-09-06 11:28:29 -0700

    hidden for revamp

--- scripts/stonks.js
+++ scripts/stonks.js
@@ -3,9 +3,12 @@ window.onscroll = function() {myFunction()};
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
+var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop +2;
+var sticky = navbar.offsetTop;
+var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+var scrolled = (winScroll / height) * 100;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
@@ -16,14 +19,6 @@ function myFunction() {
   }
 }
 
-// function myFunction() {
-// print(window.pageYOffset);
-// print(sticky);
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
-
-
+function myFunction() {
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
